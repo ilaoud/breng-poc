658b7823a3e3c1bbb98bbd49e93f7053864ea3bc
ilaoud <laoudini.imad@gmail.com>
2022-06-04 15:58:59 +0200

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class NzDemoButtonIconComponent {}
+export class NzDemoButtonIconComponent {
+
+
+  isVisible = false;
+
+  constructor() {}
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+  
+
+}
